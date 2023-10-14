1532410d0f1ce8ae3cb0f3753b27cdb876d4e63c
ryan gibbons <foo@bar.net>
2023-10-13 21:05:48 -0400

    Initial Commit

---
+++ five/fiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiveApp.swift
+//  five
+//
+//  Created by Ryan Gibbons on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct fiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
